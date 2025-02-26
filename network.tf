6c35470df662b01a3812af4b073aaccdd2d854fd
buddhika-yb <buddhika@yabie.com>
2025-02-26 12:25:50 +0530

    com2

--- network.tf
+++ network.tf
@@ -7,5 +7,6 @@ resource "google_compute_subnetwork" "subnet" {
   name          = "private-subnet"
   network       = google_compute_network.vpc.id
   region        = var.region
+  ip_cidr_range = "10.0.0.0/24"
   private_ip_google_access = true
 }
